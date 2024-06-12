bd539cdb05e432b9651dc70d4c6454402bb9f548
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-06-12 22:02:47 +0200

    Timer system added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] Canvas UI;
     [SerializeField] GameObject player;
     [SerializeField] GameObject Camera;
+    [SerializeField] TMP_Text timer;
 
     private int playerHealth;
     private int playerHealthMax;
     private int playerHealthMin;
 
     public int angleFlag = 0;
+    public float time = 60;
+    float seconds;
     
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void timeCounter()
+    {
+
     }
 
     // Update is called once per frame
@@ -34,10 +43,17 @@ public class GameManager : MonoBehaviour
 
         if (angleFlag > 3)
             angleFlag = 0;
+
     }
 
     private void FixedUpdate()
     {
-
+        time -= Time.deltaTime;
+        if (time >=60)
+            seconds = time - 60f*(int)(time/60f);
+        if (seconds >= 10)
+            timer.text = "0" + ((int)(time / 60)).ToString() + ":" + ((int)seconds).ToString();
+        else
+            timer.text = "0" + ((int)(time / 60)).ToString() + ":" + "0" + ((int)seconds).ToString();
     }
 }
